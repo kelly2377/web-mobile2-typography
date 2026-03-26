b6d78306e6997af59978b5a6085a3668a814b198
jwv2290 <181763236+jwv2290@users.noreply.github.com>
2026-03-26 16:34:57 -0400

    Main page

--- home.php
+++ home.php
@@ -11,6 +11,62 @@
     <?php include "assets/inc/nav.php"; ?>
     
     <!-- page info  -->
+    <main>
+        <div>
+            <section class="large">
+                <img class="left" src="assets/images/typography.png">
+                <div>
+                    <h2>What is Typography?</h2>
+                    <p>
+                        Typography refers to the appearance and style of text published online or in print. 
+                    </p>
+                    <p>
+                        In essence it's the art of arranging text to make it legible and aesthetically appealing. This includes carefully selecting elements such as typefaces, fonts, text size, as well as line, letter, and word spacing to generate a desired message or reaction from the reader.
+                    </p>
+                    <p>
+                        Although historically considered in reference to language, typography is also a significant aspect of UX/UI design
+                    </p>
+                    <a class="continue" href="goodvsbad/">Learn more about Good vs Bad Typography →</a>
+                </div>
+            </seciton>
+        </div>
+
+        <div>
+            <section class="small">
+                <div>
+                    <h2>Role of Typography</h2>
+                    <p>
+                        Typography plays a key role in improving the UX and UI of websites, products, and services. 
+                    </p>
+                    <p>
+                        Great use of typography in UX/UI design doesn't only improve legibility and help a reader to invest in text, it also encourages deeper exploration of a web page or website. 
+                    </p>
+                    <p>
+                        Use of typography in UX/UI design is often considered with other design aspects, such as spacing, styling, and contrast in relation to the background and other graphics.
+                    </p>
+                </div>
+            </section>
+
+            <section class="medium">
+                <div>
+                    <h2>Importance in Design</h2>
+                    <p>
+                        Typography is important in graphic design and visual communication as a whole. Its purpose is twofold: to communicate the message and to extend the overall aesthetic of the design. In this way, typography can serve as its own design element.
+                    </p>
+
+                    <h3>Role of Typography in Visual Communication</h3>
+                    <p>
+                        Typography ensures that the message comes across clearly. Designers must choose readable, legible fonts and present text in a way that properly communicates the intended sentiment and tone.
+                    </p>
+
+                    <h3>Typography as a Design Element</h3>
+                    <p>
+                        Typography can also function as its own unique design element. As discussed, different fonts can evoke different emotions, just as any other design element on the page (or screen). Typography can also help establish or build a brand identity while enhancing the overall aesthetic of a document.
+                    </p>
+                </div>
+            </section>
+        </div>
+    </main>
 
     <!-- footer -->
     <?php include "assets/inc/footer.php"; ?>
