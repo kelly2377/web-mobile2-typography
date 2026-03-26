467e5863f53ca0205370eb4c92cea9af06b6e876
jwv2290 <181763236+jwv2290@users.noreply.github.com>
2026-03-26 14:43:57 -0400

    Use php includes for nav and footer

---
+++ assets/inc/nav.php
@@ -0,0 +1,46 @@
+<nav id="primary-nav" aria-label="Primary">
+    <h1 class="nav-title">Typography</h1>
+    <button id="menu-button" type="button" aria-expanded="false" aria-controls="primary-nav-list">
+        Menu
+    </button>
+    <ul id="primary-nav-list" class="main-menu">
+        <li>
+            <a href="#">Home</a>
+        </li>
+
+        <li>
+            <button type="button" class="submenu-toggle" aria-expanded="false" aria-controls="submenu-history">
+                History
+            </button>
+            <ul id="submenu-history" class="submenu">
+                <li><a href="#">Origins & Evolution</a></li>
+                <li><a href="#">Modern Research & Discoveries</a></li>
+            </ul>
+        </li>
+
+        <li>
+            <button type="button" class="submenu-toggle" aria-expanded="false" aria-controls="submenu-fundamentals">
+                Fundamentals
+            </button>
+            <ul id="submenu-fundamentals" class="submenu">
+                <li><a href="#">Typographic Elements</a></li>
+                <li><a href="#">Principles of Typography</a></li>
+            </ul>
+        </li>
+
+
+        <li>
+            <button type="button" class="submenu-toggle" aria-expanded="false" aria-controls="submenu-uses">
+                Practical Uses
+            </button>
+            <ul id="submenu-uses" class="submenu">
+                <li><a href="#">Applications</a></li>
+                <li><a href="#">Good vs Bad Uses of Typography</a></li>
+            </ul>
+        </li>
+
+        <li>
+            <a href="#">Typographers</a>
+        </li>
+    </ul>
+</nav>
---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Typography</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+    <!-- nav -->
+    <?php include "assets/inc/nav.php"; ?>
+    
+    <!-- page info  -->
+
+    <!-- footer -->
+    <?php include "assets/inc/footer.php"; ?>
+
+    <script src="assets/js/menu.js"></script>
+    
+</body>
+</html>
