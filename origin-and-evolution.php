52c8be10dd88fe15ef8d68f3590ff8d162c440c2
Kelly <kelly.garcia.2377@gmail.com>
2026-03-31 16:16:19 -0400

    page info added

--- origin-and-evolution.php
+++ origin-and-evolution.php
@@ -1,4 +1,36 @@
 <?php
-
-
+$currentPage = "Origins & Evolution | History"
+include "assets/inc/head.php";
 ?>
+<body>
+    <!-- nav -->
+    <?php
+    $currentPage = "Origins & Evolution | History";
+    include "assets/inc/nav.php";
+    ?>
+    <!-- page info -->
+    <main>
+        <h2>Origins</h2>
+        <p>During the 20,000 B.C., the first written communication was found in an ancient cave. During the ancient civilization they used  hieroglyphs and 
+            pictograms symbols were used to communicate with others than speaking. A well known example was the cuneiform in Mesopotamia. But the pictograms 
+            symbols then evolved from drawings to abstract characters. During the medieval period, scribes would carefully create books and manuscripts by hand.
+        </p>
+        <h2>Evolution of Typography</h2>
+        <p>ypography has evolved a lot from using symbols to handwritten and illustrated manuscripts. In 3100 B.C., Egyptians were symbols and ideograms in their 
+            art, writing and architecture. In 1600 B.C. Phoenicians created phonograms/symbols that were used to represent words. Currently in the present we also 
+            use phonograms that they created such as % to represent percentage and # to represent number and many more. Phoenicians were the first to create an 
+            alphabet and in 1000 B.C. Greeks used the same alphabet. The word alphabet is a combination of the first Greek letters, alpha and beta. Romans used the 
+            Greek alphabet but they created the uppercase alphabet which is currently used. They also modified the style of letters and introduced two types of scripts,
+             formal and informal. During the middle age, handwritten and illustrated manuscripts were really popular and there was a wide range of writing styles. Art of
+            calligraphy was becoming popular and many calligraphy masters travelled across the world to share their knowledge.
+        </p>
+
+    </main>
+
+    <!-- footer -->
+    <?php include "assets/inc/footer.php"; ?>
+
+    <script src="assets/js/menu.js"></script>
+
+</body>
+</html>
