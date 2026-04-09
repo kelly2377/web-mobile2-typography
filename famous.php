9b87c4eb80866c481b1bf8dd4034733b706c644c
aharringt0n <amylharrington24@gmail.com>
2026-04-09 15:03:08 -0400

    famous typographers page

---
+++ famous.php
@@ -0,0 +1,60 @@
+<?php
+$currentPage = "Famous Typographers";
+include "assets/inc/head.php";
+?>
+<body>
+    <!-- nav -->
+    <?php
+    $currentPage = "Famous Typographers";
+    include "assets/inc/nav.php";
+    ?>
+    
+    <!-- page info  -->
+    <main>
+        <main>
+        <div>
+            <section class="large">
+                <img class="left" src="assets/images/paula.jpg">
+                <div>
+                    <h2>Paula Scher</h2>
+                    <p>
+                    Paula Scher is an American graphic designer  who has had a huge impact on design and typography. In the 90s, she worked with a public theater which redefined the visual language of cultural institutions through the use of bold typography and layered compositions that created something dynamic and alive. Her approach on that piece of work became her signature. 
+                    </p>
+                </div>
+            </seciton>
+        </div>
+
+        <div>
+            <section class="large">
+                <img class="left" src="assets/images/piet.jpg">
+                <div>
+                    <h2>Piet Zwart</h2>
+                    <p>
+                        Piet Zwart was an influential dutch designer in the twentieth century. He studied art and architecture in Amsterdam and Delft and spent many years teaching design to students. He has worked as a photographer and industrial designer and is best known for graphic design he did for a cable company and the dutch post office. He would call himself a “typotect”....
+
+                    </p>
+                </div>
+            </section>
+        </div>
+
+
+         <div>
+            <section class="large">
+                <img class="left" src="assets/images/jan.jpg">
+                <div>
+                    <h2>Jan Tschichold</h2>
+                    <p>
+                       Jan Tschichold was one of most influential figures in typography of the 20th century. His understanding of type, lettering and design led him to be able to work as an educator, designer, typeface designer and author of many books. He impacted the print industry in the Europe and the USA. His most famous typeface was Sabon created in 1967 and is still being used today. 
+                    </p>
+                </div>
+            </section>
+        </div>
+    </main>
+
+    <!-- footer -->
+    <?php include "assets/inc/footer.php"; ?>
+
+    <script src="assets/js/menu.js"></script>
+    
+</body>
+</html>
