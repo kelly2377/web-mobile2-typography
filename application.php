768bbafd9e42b34114736f1bc88081f5a82e5c49
Kelly <kelly.garcia.2377@gmail.com>
2026-04-12 21:11:02 -0400

    layout for applications page

--- application.php
+++ application.php
@@ -12,6 +12,65 @@ include "assets/inc/head.php";
     <!-- page info  -->
     <main>
         <h1>Common Typography Applications</h1>
+        <div>
+            <section class="small" style="background-color: transparent">
+            <img src="assets/images/coca_cola.png" alt="coca_cola image" style="width: 100%">
+            </section>
+            <section class="medium" style="background-color: transparent">
+                <h2>Typography for Logos</h2>
+                    <p>
+                    Kerning is the spacing between individual characters, based on their shapes. Effective kerning creates even spacing, ensuring a consistent amount of white space between letters.
+                    </p>
+            </section>
+        </div>
+
+        <div>
+            <section class="small" style="background-color: transparent">
+            <img src="assets/images/movie_poster.png" alt="little_mermaid_poster" style="width: 100%">
+            </section>
+            <section class="medium" style="background-color: transparent">
+                <h2>Typography for Posters</h2>
+                    <p>
+                    Kerning is the spacing between individual characters, based on their shapes. Effective kerning creates even spacing, ensuring a consistent amount of white space between letters.
+                    </p>
+            </section>
+        </div>
+
+        <div>
+            <section class="small" style="background-color: transparent">
+            <img src="assets/images/shirt.png" alt="fanta_t-shirt" style="width: 100%">
+            </section>
+            <section class="medium" style="background-color: transparent">
+                <h2>Typography for T-shirts </h2>
+                    <p>
+                    Kerning is the spacing between individual characters, based on their shapes. Effective kerning creates even spacing, ensuring a consistent amount of white space between letters.
+                    </p>
+            </section>
+        </div>
+
+        <div>
+            <section class="small" style="background-color: transparent">
+            <img src="assets/images/website.png" alt="application_and_website_image" style="width: 100%">
+            </section>
+            <section class="medium" style="background-color: transparent">
+                <h2>Typography for Websites & Apps </h2>
+                    <p>
+                    Kerning is the spacing between individual characters, based on their shapes. Effective kerning creates even spacing, ensuring a consistent amount of white space between letters.
+                    </p>
+            </section>
+        </div>
+
+        <div>
+            <section class="small" style="background-color: transparent">
+            <img src="assets/images/chocolate.png" alt="chocolate" style="width: 100%">
+            </section>
+            <section class="medium" style="background-color: transparent">
+                <h2>Typography for Product Packaging</h2>
+                    <p>
+                    Kerning is the spacing between individual characters, based on their shapes. Effective kerning creates even spacing, ensuring a consistent amount of white space between letters.
+                    </p>
+            </section>
+        </div>
     </main>
 
     <!-- footer -->
