c7a0b5e8dd1f82d9bf579e65827ef37379c4cddf
Kelly <kelly.garcia.2377@gmail.com>
2026-04-12 23:52:32 -0400

    finished adding info on applications page

--- application.php
+++ application.php
@@ -14,9 +14,9 @@ include "assets/inc/head.php";
         <h1>Common Typography Applications</h1>
         <div>
             <section class="large" style="background-color: transparent">
-                <img src="assets/images/coca_cola.png" alt="coca_cola_image" class="left" style="width: 40%">
+                <img src="assets/images/coca_cola.png" alt="coca_cola_image" class="left" style="width: 35%">
                 <h2>Typography for Logos</h2>
-                    <p>
+                    <p style="line-height:1.6">
                         Logos are an important fundamental pillar when it comes to strong brand identity. The Coca-Cola logo is a well known brand 
                         that is recognized everywhere. A good logo is a logo that is memorable and easily recognizable, it should also be able to 
                         stand out and create emotions in the viewer. Logos sometimes are just text and some contain both text and an icon or image.
@@ -36,7 +36,7 @@ include "assets/inc/head.php";
             <section class="large" style="background-color: transparent">
                 <img src="assets/images/movie_poster.png" alt="little_mermaid_poster" class="left" style="width: 35%">
                 <h2>Typography for Posters</h2>
-                    <p>
+                    <p style="line-height:1.6">
                         Typography in posters is important because the message on the poster will need to convey the message to the audience and need to catch 
                         the audience's eye. Posters have multiple purposes such as promoting events like concerts, local markets, albums and many other events. 
                         The poster would need to make the audience stop and read what is shown on the poster and it would also need to show the readers what the
@@ -58,14 +58,15 @@ include "assets/inc/head.php";
             <section class="large" style="background-color: transparent">
                 <img src="assets/images/shirt.png" alt="fanta_t-shirt" class="left" style="width: 35%">
                 <h2>Typography for T-shirts</h2>
-                    <p>
+                    <p style="line-height:1.6">
                         Typography for shirts are important because the typography can have different tasks such as just displaying the brand name but 
                         also it can deliver an important slogan or funny message. For instance, the "Girls support girls" slogan t-shirt from the clothing brand
                         pieces has a cursive script typeface which is an elegant text and it's positions in the center of the shirt which stands out. 
                         Some tips and best practices when it comes to typography for shirts is to know who the audience is and the kind of style preferences the
                         audience would have whether it's business casual or clothes for festivals. T-shirts that have typography are worn in order to make a statement 
                         and lettering can be used to design something different. Also, a t-shirt has less text than a website or poster so there's more freedom with the 
-                        shirt design and there could be drawings or signs on the shirt other than text.   
+                        shirt design and there could be drawings or signs on the shirt other than text. T-shirts that have the best typography have only one peice of text 
+                        and a plain background. Most use whitespace and have empty spaces between and around different design elements which helps make the message stand out. 
                     </p>
                 <div style="clear: both"></div>
             </section>
@@ -73,10 +74,17 @@ include "assets/inc/head.php";
 
         <div>
             <section class="large" style="background-color: transparent">
-                <img src="assets/images/website.png" alt="application_and_website_image" class="left" style="width: 40%">
+                <img src="assets/images/n26website.png" alt="application_and_website_image" class="left" style="width: 35%; height:auto">
                 <h2>Typography for Websites & Apps</h2>
-                    <p>
-                        Typography for website and applications is important because
+                    <p style="line-height:1.6">
+                        Typography for website and applications is important because it impacts user experience. When it comes to design a website or application, the overall goals is to create something
+                        that is user friendly and also looks easy to navigate. Users not only want to see that the website is appealing but they also want to be able to use the website or application easily. 
+                        If the website was difficult to use then the users experience would be really bad and they wouldn't want to use the website anymore. The accessibilty in a website and application also matters
+                        because some users might be colorblind so choosing the colors is very important. Also, text is very helpful in making it accessible to users. Text would help guide users around the website or application 
+                        by providing instructions and information on how to help them move from one place to another. Some tips and best practices for typography in websites and applications is to ensure that the text is readable. 
+                        Also, it's important to prioritise accessibility by choosing easy to read typefaces especially for large amount of texts. Sans-serif typerface is mostly used in website and applications because of that reason.
+                        It's also very important to know how the text would look like on different screens such as mobile devices. Before releasing a website or application, it would be helpful to test your typography and make sure that 
+                        it displays as intended on each page and screen.
                     </p>
                 <div style="clear: both"></div>
             </section>
@@ -84,10 +92,15 @@ include "assets/inc/head.php";
 
         <div>
             <section class="large" style="background-color: transparent">
-                <img src="assets/images/chocolate.png" alt="chocolate" class="left" style="width: 40%">
+                <img src="assets/images/chocolate.png" alt="chocolate" class="left" style="width: 35%">
                 <h2>Typography for Product Packaging</h2>
-                    <p>
-                        Typography for product packaging is important because 
+                    <p style="line-height:1.6">
+                        Typography for product packaging is important because it helps convey the brand's identity and help market the product to a specific audience. But typography on products is also condence because there's only a limited 
+                        space such as chocolate bar wrappers, cosmetic bottles and soda cans. Designers would need to know what information the customers would want to find when looking at a product in order to make the product stand out. Some tips and 
+                        best practices for typography in product packaging is look at what the brands values are and the market they are competing in. They would also need to know what their target audience is and what the audience would expect from the product.
+                        If the designer is designing the logo then the logo would be at the front of the packaging and would need to have a unique typeface. If the packaging was listing the ingredients then the typeface would need to be simple and would need to be
+                        readable even if it's small and in the back of the packaging. It's important to place the important information at the front and the extra details on the back and make sure that it's organized. In the front, the brand name and product name would need 
+                        to be visible on the front and on the back would be extra information such as ingredients or instructions.
                     </p>
                 <div style="clear: both"></div>
             </section>
