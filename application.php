15779288763c289b6ccbd5635b3bb7c1b65fa81b
Kelly <kelly.garcia.2377@gmail.com>
2026-04-12 22:57:34 -0400

    some info on applications page

--- application.php
+++ application.php
@@ -13,64 +13,86 @@ include "assets/inc/head.php";
     <main>
         <h1>Common Typography Applications</h1>
         <div>
-            <section class="small" style="background-color: transparent">
-            <img src="assets/images/coca_cola.png" alt="coca_cola image" style="width: 100%">
-            </section>
-            <section class="medium" style="background-color: transparent">
+            <section class="large" style="background-color: transparent">
+                <img src="assets/images/coca_cola.png" alt="coca_cola_image" class="left" style="width: 40%">
                 <h2>Typography for Logos</h2>
                     <p>
-                    Kerning is the spacing between individual characters, based on their shapes. Effective kerning creates even spacing, ensuring a consistent amount of white space between letters.
+                        Logos are an important fundamental pillar when it comes to strong brand identity. The Coca-Cola logo is a well known brand 
+                        that is recognized everywhere. A good logo is a logo that is memorable and easily recognizable, it should also be able to 
+                        stand out and create emotions in the viewer. Logos sometimes are just text and some contain both text and an icon or image.
+                        Some tips and best practices when it comes to typography design for logos is that you would need to get a clear understanding 
+                        of the branch in order to identify the typeface and color choices of the brand. You would also need to understand what the 
+                        brand's values are and what adjectives would best describe the brand. When it comes to deciding the typeface, there are three 
+                        different categories: serif typefaces such as times new roman, san serif typefaces such as arial and script typeface such as 
+                        quintessential. You would need to choose the typeface that aligns with the brand and make sure that it's easy to read. 
+                        If the brand has a color palette then it would be best to use those colors for the typography. If there isn't any color 
+                        palette then you would need to think back about the brand's values and personality. 
                     </p>
+                <div style="clear: both"></div>
             </section>
         </div>
 
         <div>
-            <section class="small" style="background-color: transparent">
-            <img src="assets/images/movie_poster.png" alt="little_mermaid_poster" style="width: 100%">
-            </section>
-            <section class="medium" style="background-color: transparent">
+            <section class="large" style="background-color: transparent">
+                <img src="assets/images/movie_poster.png" alt="little_mermaid_poster" class="left" style="width: 35%">
                 <h2>Typography for Posters</h2>
                     <p>
-                    Kerning is the spacing between individual characters, based on their shapes. Effective kerning creates even spacing, ensuring a consistent amount of white space between letters.
+                        Typography in posters is important because the message on the poster will need to convey the message to the audience and need to catch 
+                        the audience's eye. Posters have multiple purposes such as promoting events like concerts, local markets, albums and many other events. 
+                        The poster would need to make the audience stop and read what is shown on the poster and it would also need to show the readers what the
+                        most important parts of the poster are. For instance, in the Little mermaid poster, the title stands out. Some tips and practices for 
+                        typography for posters is to know what the main message you want to convey is and choose the appropriate typeface. You would need to define
+                        the most important texts that you would want to show the audience and then the secondary and tertiary information that doesn't need to be 
+                        noticeable but can be seen if the person were to look closer. When it comes to typography in posters, you have more options of typefaces and
+                        fonts and it would help show which information is more important. Sometimes you would choose a bold and differnt typeface for the main message
+                        such as movie title or name of the band or tour/album name. Then you would choose a more cleaner and smaller text for the other information such
+                        as the date or the actress name. Hierachy is important when it comes to typography in posters because it helps you figure out where to position
+                        and style the poster. You would want to have the poster headline and main message in the center or top and it would need to be bold and big. The
+                        secondary information would be position lowered down in the poster and the more important information would be surrounded by whitespace.
                     </p>
+                <div style="clear: both"></div>
             </section>
         </div>
 
         <div>
-            <section class="small" style="background-color: transparent">
-            <img src="assets/images/shirt.png" alt="fanta_t-shirt" style="width: 100%">
-            </section>
-            <section class="medium" style="background-color: transparent">
-                <h2>Typography for T-shirts </h2>
+            <section class="large" style="background-color: transparent">
+                <img src="assets/images/shirt.png" alt="fanta_t-shirt" class="left" style="width: 35%">
+                <h2>Typography for T-shirts</h2>
                     <p>
-                    Kerning is the spacing between individual characters, based on their shapes. Effective kerning creates even spacing, ensuring a consistent amount of white space between letters.
+                        Typography for shirts are important because the typography can have different tasks such as just displaying the brand name but 
+                        also it can deliver an important slogan or funny message. For instance, the "Girls support girls" slogan t-shirt from the clothing brand
+                        pieces has a cursive script typeface which is an elegant text and it's positions in the center of the shirt which stands out. 
+                        Some tips and best practices when it comes to typography for shirts is to know who the audience is and the kind of style preferences the
+                        audience would have whether it's business casual or clothes for festivals. T-shirts that have typography are worn in order to make a statement 
+                        and lettering can be used to design something different. Also, a t-shirt has less text than a website or poster so there's more freedom with the 
+                        shirt design and there could be drawings or signs on the shirt other than text.   
                     </p>
+                <div style="clear: both"></div>
             </section>
         </div>
 
         <div>
-            <section class="small" style="background-color: transparent">
-            <img src="assets/images/website.png" alt="application_and_website_image" style="width: 100%">
-            </section>
-            <section class="medium" style="background-color: transparent">
-                <h2>Typography for Websites & Apps </h2>
+            <section class="large" style="background-color: transparent">
+                <img src="assets/images/website.png" alt="application_and_website_image" class="left" style="width: 40%">
+                <h2>Typography for Websites & Apps</h2>
                     <p>
-                    Kerning is the spacing between individual characters, based on their shapes. Effective kerning creates even spacing, ensuring a consistent amount of white space between letters.
+                        Typography for website and applications is important because
                     </p>
+                <div style="clear: both"></div>
             </section>
         </div>
 
         <div>
-            <section class="small" style="background-color: transparent">
-            <img src="assets/images/chocolate.png" alt="chocolate" style="width: 100%">
-            </section>
-            <section class="medium" style="background-color: transparent">
+            <section class="large" style="background-color: transparent">
+                <img src="assets/images/chocolate.png" alt="chocolate" class="left" style="width: 40%">
                 <h2>Typography for Product Packaging</h2>
                     <p>
-                    Kerning is the spacing between individual characters, based on their shapes. Effective kerning creates even spacing, ensuring a consistent amount of white space between letters.
+                        Typography for product packaging is important because 
                     </p>
+                <div style="clear: both"></div>
             </section>
         </div>
+        <button>Take Quiz!</button> 
     </main>
 
     <!-- footer -->
