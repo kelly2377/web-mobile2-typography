0833992b9ad760035f8a56df79cd4e1e423fb79d
Kaila Grant <kailagrant141@gmail.com>
2026-04-21 19:31:43 -0400

    navigation styling is now fixed and consistent

--- assets/inc/nav.php
+++ assets/inc/nav.php
@@ -25,8 +25,8 @@
                 Fundamentals
             </button>
             <ul id="submenu-fundamentals" class="submenu">
-                <li <?php if ($currentPage == "Typographical Elements") { echo 'class="is-current"'; } ?>>><a href="typographical_elements.php">Typographic Elements</a></li>
-                <li <?php if ($currentPage == "Principles of Typography") { echo 'class="is-current"'; } ?>>><a href="principles.php">Principles of Typography</a></li>
+                <li <?php if ($currentPage == "Typographical Elements") { echo 'class="is-current"'; } ?>><a href="typographical_elements.php">Typographic Elements</a></li>
+                <li <?php if ($currentPage == "Principles of Typography") { echo 'class="is-current"'; } ?>><a href="principles.php">Principles of Typography</a></li>
             </ul>
         </li>
 
